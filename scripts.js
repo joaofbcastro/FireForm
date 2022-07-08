a816a6f7367192f27db65004b251f5392c2198b6
João Castro <joaobomfim7@hotmail.com>
2022-07-07 23:12:48 -0300

    Adiciona a funcionalidade de excluir postagens

--- scripts.js
+++ scripts.js
@@ -1,4 +1,10 @@
-function openModal() {
+function getRandomInt (min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); 
+}
+
+function toggleModal () {
     const modal = document.querySelector('#modal');
     if (modal.classList.contains('modal-open')) {
         modal.classList.remove('modal-open');
@@ -6,15 +12,55 @@ function openModal() {
         modal.classList.add('modal-open');
     }
 }
-
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); 
-}
   
-const newPostForm = document.getElementById('newPost')
-newPostForm.addEventListener('submit', (e) => {
+const insertPost = (post) => {
+    const postList = document.querySelector('.postList')
+
+    const closeButton = document.createElement('button');
+    closeButton.classList.add('button-close');
+    closeButton.setAttribute('postid', post.postId);
+    closeButton.addEventListener('click', () => removePost(post.postId));
+
+    const postMessage = document.createElement('p');
+    postMessage.classList.add('postMessage');
+    postMessage.textContent = post.message;
+
+    const postAuthorName = document.createElement('h3');
+    postAuthorName.classList.add('postAuthorName');
+    postAuthorName.textContent = post.author;
+
+    const div = document.createElement('div');
+    div.insertAdjacentElement('beforeend', postAuthorName);
+    div.insertAdjacentElement('beforeend', postMessage);
+
+    const postAuthorAvatar = document.createElement('img');
+    postAuthorAvatar.classList.add('postAuthorAvatar');
+    postAuthorAvatar.src = post.authorAvatar;
+
+    const postItem = document.createElement('li');
+    postItem.classList.add('postItem');
+    postItem.insertAdjacentElement('beforeend', postAuthorAvatar);
+    postItem.insertAdjacentElement('beforeend', div);
+    postItem.insertAdjacentElement('beforeend', closeButton);
+
+    postList.insertAdjacentElement('afterbegin', postItem);
+}
+
+const updateLocalStorage = (list) => {
+    localStorage.setItem('postList', JSON.stringify(list));
+}
+
+const localStoragePostList = JSON.parse(localStorage.getItem('postList'));
+let postList = localStorage.getItem('postList') !== null ? localStoragePostList : []
+
+postList.forEach((post) => {
+    insertPost(post)
+})
+
+const postForm = document.getElementById('newPost')
+postForm.addEventListener('submit', addNewPost)
+
+function addNewPost(e) {
     const post = {
         author: `Usuário #${getRandomInt(1000, 9999)}`,
         message: e.target.message.value,
@@ -23,43 +69,16 @@ newPostForm.addEventListener('submit', (e) => {
     }
     postList.push(post)
     updateLocalStorage(postList)
-    openModal()
-})
-
-function createPost(post) {
-    const postList = document.querySelector('.postList')
-
-    const p = document.createElement('p');
-    p.classList.add('postMessage');
-    p.textContent = post.message;
-
-    const h3 = document.createElement('h3');
-    h3.classList.add('postAuthorName');
-    h3.textContent = post.author;
-
-    const div = document.createElement('div');
-    div.insertAdjacentElement('beforeend', h3);
-    div.insertAdjacentElement('beforeend', p);
-
-    const img = document.createElement('img');
-    img.classList.add('postAuthorAvatar');
-    img.src = post.authorAvatar;
-
-    const li = document.createElement('li');
-    li.classList.add('postItem');
-    li.insertAdjacentElement('beforeend', img);
-    li.insertAdjacentElement('beforeend', div);
-
-    postList.insertAdjacentElement('afterbegin', li);
+    toggleModal()
 }
 
-function updateLocalStorage(list) {
-    localStorage.setItem('postList', JSON.stringify(postList));
+function removePost(post) {
+    postList.forEach(itemPost => {
+        if (post === itemPost.postId) {
+            let index = postList.indexOf(itemPost);
+            postList.splice(index, 1);
+            updateLocalStorage(postList)
+            location.reload()
+        }
+    })
 }
-
-const localStoragePostList = JSON.parse(localStorage.getItem('postList'));
-let postList = localStorage.getItem('postList') !== null ? localStoragePostList : []
-
-postList.forEach((post) => {
-    createPost(post)
-})
